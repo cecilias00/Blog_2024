789076766c7dcf93666530aa077bb7a4a65cf886
Cecilia <cecilia@MacBook-Air-de-Cecilia.local>
2024-10-01 01:30:59 -0300

    Descripción de los cambios

--- Blog/Apps/posts/admin.py
+++ Blog/Apps/posts/admin.py
@@ -1,7 +1,35 @@
 from django.contrib import admin
 from .models import Posts, Categorias, Comentarios
+from unidecode import unidecode
+from django.db.models import Q
 
-# Register your models here.
-admin.site.register(Posts)
+class PostsAdmin(admin.ModelAdmin):
+    list_display = ("titulo", "fecha_publicacion", "categoria")
+    search_fields = ("titulo", "categoria__nombre")
+    list_filter = ("fecha_publicacion",)
+
+    def get_search_results(self, request, queryset, search_term):
+        # Normalizar el término de búsqueda
+        search_term_normalized = unidecode(search_term).strip().lower()
+
+        # Realizar la búsqueda en el queryset original
+        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
+
+        # Filtrar resultados con acentos
+        queryset |= self.model.objects.filter(
+            Q(titulo__icontains=search_term) |
+            Q(categoria__nombre__icontains=search_term)
+        )
+
+        # Filtrar resultados sin acentos
+        queryset |= self.model.objects.filter(
+            Q(titulo__icontains=search_term_normalized) |
+            Q(categoria__nombre__icontains=search_term_normalized)
+        ).exclude(id__in=queryset.values_list('id', flat=True))
+
+        return queryset, use_distinct
+
+# Registra tus modelos aquí.
+admin.site.register(Posts, PostsAdmin)
 admin.site.register(Categorias)
 admin.site.register(Comentarios)
